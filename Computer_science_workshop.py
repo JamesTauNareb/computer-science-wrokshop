b8a91121add1e3e0fb7a1dccfdce5ec30cf98ffa
Piet <my email>
2024-09-07 13:23:50 +0200

    messsage

---
+++ Computer_science_workshop.py
@@ -0,0 +1,45 @@
+num1 = int(input('Enter first number:'))
+num2 = int(input('Enter second number:'))
+
+print(num1 + num2)
+
+def calc(num1, num2, opr):
+    if oppr == '+':
+    print(num1 + num2)
+    elif opr == '-':
+    print(num1 - num1)
+    elif opr == '*':
+     print(num1 * num2)
+    elif opr == '/':
+     print(num1 / num2)
+    else:
+     print('Invalid input')
+def calc (8, 9, '*')
+
+class Calulator:
+    def__init__(self, x, y, opr):
+        self.num1 = x
+        self.num2 = y
+        self.opr = opr
+
+def evaluation(self)
+if self.opr == '+':
+        print(self.num1 + self.num2)
+    elif self.opr == '-':
+         print(sself.num1 - self.num2)
+    elif self.opr == '*':
+        print(self.num1 * self.num2)
+    elif self.opr == '/':
+        print(self.num1 / self.num2)
+     else:
+        print('Invalid input')
+
+
+ expr = Calculator(8, 9, '*')
+
+ print(expr.num1)
+
+
+
+
+    
